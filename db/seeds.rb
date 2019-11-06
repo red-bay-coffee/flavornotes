1821c6fe6e55e4021dfad50e29d1a162443ba3c9
Jonathan Nye <jonathan.m.nye@gmail.com>
2019-11-05 22:37:18 -0800

    Add seed data for coffees and flavor notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coffee_mapping = {
+  'Brazilian Cake Lady' => ['Citrus Zest', 'Golden Raisin', 'Nougat'],
+  'Carver’s Dream' => ['Almond Paste', 'Cherry Compote', 'Dark Chocolate'],
+  'Coltrane' => ['Black Grape', 'Cane Sugar', 'Dark Chocolate'],
+  'East 14th' => ['Blackberry', 'Dark Chocolate', 'Date'],
+  'King’s Prize' => ['Honeysuckle', 'Melon', 'White Peach'],
+  'Mexico Chiapas Bella Vista' => ['Asian Pear', 'Cinnamon', 'Milk Chocolate'],
+  'Ocho Libre' => ['Caramel', 'Cinnamon', 'Tangerine'],
+  'Pretty Penny' => ['Caramelized Peach', 'Semi-sweet Chocolate', 'White Grape'],
+  'Slow Burn' => ['Allspice', 'Mission Fig', 'Molasses']
+}
+
+coffee_mapping.each do |coffee_name, flavor_note_names|
+  flavor_notes = flavor_note_names.map do |note_name|
+    FlavorNote.find_or_create_by(name: note_name)
+  end
+  coffee = Coffee.find_or_create_by(name: coffee_name)
+  coffee.update(flavor_notes: flavor_notes)
+end
