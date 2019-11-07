c5daee364cf71d9ea65f5ee3c3c886d679c91cd9
Jonathan Nye <jonathan.m.nye@gmail.com>
2019-11-06 23:01:37 -0800

    Add show action for users after create that displays their preferred coffee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,18 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { render '/users/new' }
+        format.html { redirect_to @user }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def show
+    @user = User.find params[:id]
+    @coffee = Coffee.find_by_flavor_note(@user.flavor_notes.first)
   end
 
   private
